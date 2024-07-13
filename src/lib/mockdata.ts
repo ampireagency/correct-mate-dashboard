d2487334903cd185140e98aa0b07354d6e08b1d6
timothytim812 <timothygeorgebenjamindas@gmail.com>
2024-07-13 21:27:03 +0530

    init

--- src/lib/mockdata.ts
+++ src/lib/mockdata.ts
@@ -6,7 +6,7 @@ export const mockData = {
       sender: "system",
       timestamp: "2024-07-13T10:00:00Z",
       message:
-        "Hello! Here are your results for the recent assessment.\n 1. Describe Business Law ? \n Successful businesses prioritize innovation and customer-centric strategies to stay ahead. From agile startups to established corporations, effective leadership and strategic planning are pivotal. Leveraging data analytics and digital marketing, businesses optimize operations and enhance customer engagement. Moreover, fostering a diverse and inclusive workplace cultivates creativity and drives sustainable growth. Embracing these principles fosters resilience and propels businesses towards achieving long-term success in dynamic global markets",
+        "Hello! Here are your results for the recent assessment.\n 1 . Describe Business Law ? \n Successful businesses prioritize innovation and customer-centric strategies to stay ahead. From agile startups to established corporations, effective leadership and strategic planning are pivotal. Leveraging data analytics and digital marketing, businesses optimize operations and enhance customer engagement. Moreover, fostering a diverse and inclusive workplace cultivates creativity and drives sustainable growth. Embracing these principles fosters resilience and propels businesses towards achieving long-term success in dynamic global markets",
     },
   ],
 };
